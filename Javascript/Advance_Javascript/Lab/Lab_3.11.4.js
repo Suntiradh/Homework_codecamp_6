dbdd10d66518f24a0537d8e405a3d11fb9dfd00b
Suntiradh <suntiradhkesbungkan@gmail.com>
2020-08-15 14:25:50 +0700

    Update Lab 3.11.14 and 4.10.10 done

---
+++ Javascript/Advance_Javascript/Lab/Lab_3.11.4.js
@@ -0,0 +1,20 @@
+function getMaxSubSum(arr) {
+    let maxSum = 0; // if we take no elements, zero will be returned
+  
+    for (let i = 0; i < arr.length; i++) {
+      let sumFixedStart = 0;
+      for (let j = i; j < arr.length; j++) {
+        sumFixedStart += arr[j];
+        maxSum = Math.max(maxSum, sumFixedStart);
+      }
+    }
+  
+    return maxSum;
+  }
+  
+  console.log(getMaxSubSum([-1, 2, 3, -9]) );
+  console.log(getMaxSubSum([2, -1, 2, 3, -9]) );
+  console.log(getMaxSubSum([-1, 2, 3, -9, 11]) );
+  console.log(getMaxSubSum([-2, -1, 1, 2]) );
+  console.log(getMaxSubSum([100, -9, 2, -3, 5]) );
+  console.log(getMaxSubSum([1, 2, 3]) );
---
+++ Javascript/Advance_Javascript/Lab/Lab_4.10.10.js
@@ -0,0 +1,5 @@
+let array1 = [100, 200.25, 300.84, 400.3];
+let array2 = array1.map(function(x) {
+       return x.toFixed(2); 
+});
+console.log(array2);
