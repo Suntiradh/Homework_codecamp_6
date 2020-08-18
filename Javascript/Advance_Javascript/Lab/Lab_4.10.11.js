a1e19b4272f56dbc7f3213e1f24ac5bb99927a9c
Suntiradh <suntiradhkesbungkan@gmail.com>
2020-08-18 21:29:39 +0700

    Update Lab 4.10.11.js done

---
+++ Javascript/Advance_Javascript/Lab/Lab_4.10.11.js
@@ -0,0 +1,9 @@
+let array1 = [
+    { name: "apple", birth: "2000-01-01" },
+    { name: "banana", birth: "1990-10-01" },
+    { name: "watermelon", birth: "1985-12-01" },
+]
+    ;
+let array2 = array1.map((x, k) => ({ ...x, age: (new Date().getFullYear()) - (new Date(array1[k].birth).getFullYear()) }))
+
+console.log(array2);
